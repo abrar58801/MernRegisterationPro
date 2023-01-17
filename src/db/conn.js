b5d8fb5d05974e0b01314f5da97748b4b1fa5b93
abrar ahmad <abrar.web.developer@gmail.com>
2023-01-17 22:47:41 +0530

    some edit

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // mongodb connection here
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://localhost:27017/registeration").then(() =>{
+mongoose.connect(process.env.DB_PATH).then(() =>{
     console.log("db Connection Successful");
 }).catch((e) =>{
     console.log("Connection Failed");
